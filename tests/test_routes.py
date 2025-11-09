21c6b27f8b43c302ca7a728876b5396a6c82b86d
Dimple96-web <dimpledechamma96@gmail.com>
2025-11-09 11:32:47 +0530

    updated the test cases

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,7 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app, calculate_bmi, calculate_calories
+
 
 def test_home_route():
     client = app.test_client()
